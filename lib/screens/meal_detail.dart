29175c7cd240df955ce7d7cf4dc43a78a60092af
Joao Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-11-20 12:59:35 -0300

    adding ingredients and steps to meal detail screen

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -5,6 +5,30 @@ class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal_detail';
   const MealDetailScreen({Key? key}) : super(key: key);
 
+  Widget buildSectionTitle(BuildContext ctx, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(ctx).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget buildContainer(Widget child) {
+    return Container(
+      height: 200,
+      width: 300,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Meal meal = ModalRoute.of(context)!.settings.arguments as Meal;
@@ -12,6 +36,58 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(meal.imageUrl),
+            ),
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, idx) => Card(
+                  elevation: 4,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(
+                      meal.ingredients[idx],
+                      style: const TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  ),
+                ),
+                itemCount: meal.ingredients.length,
+              ),
+            ),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(ListView.builder(
+              itemBuilder: (ctx, idx) => Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('# ${idx + 1}'),
+                    ),
+                    title: Text(
+                      meal.steps[idx],
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  if (idx != meal.steps.length - 1)
+                    const Divider(
+                      thickness: 1,
+                    ),
+                ],
+              ),
+              itemCount: meal.steps.length,
+            ))
+          ],
+        ),
       ),
     );
   }
